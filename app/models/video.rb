cb2cfc7b126c4e073b3aa0ffc1625d8bfd2567b2
Gregory Mayer <gregory.nicholas.mayer@hotmail.com>
2021-02-03 04:36:41 -0300

    add name field to devise user

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,4 @@
 class Video < ApplicationRecord
   belongs_to :user
   has_one_attached :video_file
-  # has_one_attached :thumbnail
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_030210) do
+ActiveRecord::Schema.define(version: 2021_02_03_054926) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_02_03_030210) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "teacher", default: false
     t.string "username"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
