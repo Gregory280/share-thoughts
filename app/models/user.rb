56be5903e5c243c8883a1c18cf397715d3e28dc2
Gregory Mayer <gregory.nicholas.mayer@hotmail.com>
2021-02-05 01:05:54 -0300

    modifying erros messages and style

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,19 @@ class User < ApplicationRecord
 
   has_many :videos
 
+  validates :password, presence: { message: 'Senha não pode ser vazio.' }
+
   validates :email, 
     uniqueness: { message: 'Email já cadastrado.' }
 
   validates :username,
     uniqueness: { message: 'Nome de Usuário já cadastrado.' },
     length: { in: 3..25, 
-      message: 'Nome de usuário com tamanho inválido.' }
+      message: 'Nome de usuário com tamanho inválido.' },
+    presence: { message: 'Nome de usuário não pode ser vazio.' }
 
   validates :name,
     length: { in: 3..50,
-      message: 'Nome com tamanho inválido.' }
+      message: 'Nome com tamanho inválido.' },
+    presence: { message: 'Nome não pode ser vazio.' }
 end
