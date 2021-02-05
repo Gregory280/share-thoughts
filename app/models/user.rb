1aa58c34f1ecdcca2d7ec71ddcac95ffe684b9cd
Gregory Mayer <gregory.nicholas.mayer@hotmail.com>
2021-02-04 22:00:01 -0300

    Add models validations and error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :videos
+
+  validates :email, 
+    uniqueness: { message: 'Email já cadastrado.' }
+
+  validates :username,
+    uniqueness: { message: 'Nome de Usuário já cadastrado.' },
+    length: { in: 3..25, 
+      message: 'Nome de usuário com tamanho inválido.' }
+
+  validates :name,
+    length: { in: 3..50,
+      message: 'Nome com tamanho inválido.' }
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,13 @@
 class Video < ApplicationRecord
   belongs_to :user
   has_one_attached :video_file
+  validates :title, 
+    length: { in: 3..150, message: 'O título deve ser conciso.' },
+    presence: true
+  validates :description,
+    length: { in: 3..1000, message: 'Dê um resumo de sua aula.' },
+    presence: true
+  validates :video_file, presence: true,
+    content_type: [:mp4], size: { less_than: 600.megabytes, 
+      message: 'Seu vídeo ultrapassou o tamanho limite.'}
 end
