106094498cff94f59e1e8b23df7019b309b63779
Gregory Mayer <gregory.nicholas.mayer@hotmail.com>
2021-02-08 02:00:59 -0300

    Fix helper method is_teacher

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :is_teacher?
   
-  
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :about])
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def is_teacher?
-    current_user && current_user.teacher == true
+    current_user && current_user.teacher === true
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @likes = count_videos_likes(@user)
+    @comments = count_videos_comments(@user)
   end
 
   def index
@@ -10,6 +11,15 @@ class UsersController < ApplicationController
 
 
 private
+
+  def count_videos_comments(user)
+    user_videos = user.videos
+    count_comments = 0
+    user.videos.each do |video|
+      count_comments = count_comments + video.comments.count
+    end
+    @comments = count_comments
+  end
 
   def count_videos_likes(user)
     user_videos = user.videos
