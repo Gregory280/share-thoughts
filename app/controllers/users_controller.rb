c502f9d216f965725212ac726199b1d7621364c5
Gregory Mayer <gregory.nicholas.mayer@hotmail.com>
2021-02-08 01:44:42 -0300

    Updating teachers profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-   
+    @likes = count_videos_likes(@user)
   end
 
   def index
@@ -10,5 +10,14 @@ class UsersController < ApplicationController
 
 
 private
+
+  def count_videos_likes(user)
+    user_videos = user.videos
+    count_likes= 0
+    user_videos.each do |video|
+      count_likes = count_likes + video.likes.count
+    end
+    @likes = count_likes
+  end
 
 end
